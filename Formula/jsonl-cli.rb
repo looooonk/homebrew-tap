3b688ed90992ebe2a8e0e0c7c407703dedcff31f
looooonk <cadenhwang05@gmail.com>
2026-02-01 18:05:47 -0500

    content: updated checksum

--- Formula/jsonl-cli.rb
+++ Formula/jsonl-cli.rb
@@ -4,7 +4,7 @@ class JsonlCli < Formula
   desc "CLI JSONL viewer"
   homepage "https://github.com/looooonk/jsonl-viewer"
   url "https://github.com/looooonk/jsonl-viewer/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "bfd14cd3a1f89e189385f4629168ec0035305a876f042aa810d15c491878ac62"
   license "MIT"
 
   depends_on "python@3.12"
