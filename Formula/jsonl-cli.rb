df9c59a738f49c46d1c2ce1281c38d0cf0176ab7
looooonk <cadenhwang05@gmail.com>
2026-02-01 17:29:59 -0500

    content: fixed links

--- Formula/jsonl-cli.rb
+++ Formula/jsonl-cli.rb
@@ -2,8 +2,8 @@ class JsonlCli < Formula
   include Language::Python::Virtualenv
 
   desc "CLI JSONL viewer"
-  homepage "https://github.com/looooonk/jsonl-cli"
-  url "https://github.com/looooonk/jsonl-cli/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/looooonk/jsonl-viewer"
+  url "https://github.com/looooonk/jsonl-viewer/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
